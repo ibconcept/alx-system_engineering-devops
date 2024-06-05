a3da7c5344c550c2a9c440d2be29a33065836573
William Maina <willima09@gmail.com>
2024-06-05 10:07:35 -0700

    update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
 
-"""
-prints the titles of the first 10 hot posts listed for a given subreddit
-"""
-
 from requests import get
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
+    Function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
     try:
-        my_data = results.get('data').get('children')
+        response = get(url, headers=user_agent, params=params)
+        if response.status_code != 200:
+            print("None")
+            return
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+        results = response.json()
+        my_data = results.get('data', {}).get('children', [])
 
-    except Exception:
+        if not my_data:
+            print("None")
+            return
+
+        for post in my_data:
+            print(post.get('data', {}).get('title', 'None'))
+
+    except Exception as e:
         print("None")
+
+
+# Example usage
+top_ten("python")
+
