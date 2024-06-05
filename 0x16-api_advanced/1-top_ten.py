c7e3a57de2751616fde8d0ce42b0b1aa154d2b32
William Maina <willima09@gmail.com>
2024-06-05 11:43:46 -0700

    update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,42 +1,40 @@
+#!/usr/bin/python3
+'''
+    This module contains the function top_ten.
+'''
+
 import requests
+from sys import argv
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-
-    Args:
-    subreddit (str): The name of the subreddit.
-
-    Returns:
-    None
-    """
-    # Set a generic user-agent header
-    headers = {'User-Agent': 'MyRedditScript/1.0'}
-
-    # Construct the URL for the API request
+    '''
+    Returns the titles of the first 10 hot posts for a given subreddit.
+    '''
+    user_agent = {'User-Agent': 'MyRedditBot/1.0'}
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
-
+    
     try:
-        # Send a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if the response is successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-
-            # Extract and print the titles of the first 10 hot posts
-            for post in data.get('data', {}).get('children', [])[:10]:
-                print(post.get('data', {}).get('title'))
-        else:
-            # Print None if the subreddit is not valid or there is another issue
-            print("None")
+        response = requests.get(url, headers=user_agent, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception for any HTTP error status codes
+        
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        
+        if not posts:
+            print("No posts found for the subreddit:", subreddit)
+            return
+        
+        for post in posts:
+            print(post.get('data', {}).get('title'))
+    
+    except requests.HTTPError as e:
+        print("Error accessing subreddit:", e)
     except Exception as e:
-        # Print None if there is an exception
-        print("None")
+        print("An error occurred:", e)
 
-# Example usage
 if __name__ == "__main__":
-    subreddit = input("Enter the name of the subreddit: ")
-    top_ten(subreddit)
+    if len(argv) != 2:
+        print("Usage: python3 script.py subreddit_name")
+    else:
+        top_ten(argv[1])
 
