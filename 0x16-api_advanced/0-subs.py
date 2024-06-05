836b0513242c91f468900aeafc7fbb44cffe2c6a
William Maina <willima09@gmail.com>
2024-06-05 12:08:47 -0700

    update code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "python:com.example.redditapi:v1.0.0 (by u/your_username)"
+        "User-Agent": "python:com.example.redditapi:v1.0.0 (by maina)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200 and not response.is_redirect:
