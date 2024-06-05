77c7fc0515c990fa2447dc48266aff98d49bb7ac
William Maina <willima09@gmail.com>
2024-06-05 11:50:38 -0700

    update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 '''
-Module for retrieving the top ten posts from a subreddit.
+    This module contains the function top_ten
 '''
+
 import requests
-import sys
+from sys import argv
+
 
 def top_ten(subreddit):
     '''
-    Retrieves the top ten posts for a given subreddit.
+        Returns the top ten posts for a given subreddit
     '''
+
     user_agent = {'User-Agent': 'MyRedditScript/1.0'}
+
+    url = ('https://www.reddit.com/r/{}/hot/.json?'
+           'limit=10'.format(subreddit))
+
     try:
-        response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10',
-                                headers=user_agent)
+        response = requests.get(url, headers=user_agent)
         response.raise_for_status()  # Raise an exception for HTTP errors
+
         data = response.json()
+
         for post in data.get('data').get('children'):
             print(post.get('data').get('title'))
+
     except requests.HTTPError as e:
         print("HTTP error occurred:", e)
     except requests.RequestException as e:
@@ -24,10 +33,10 @@ def top_ten(subreddit):
     except Exception as e:
         print("An unexpected error occurred:", e)
 
+
 if __name__ == "__main__":
-    subreddit_name = sys.argv[1] if len(sys.argv) > 1 else None
+    subreddit_name = argv[1] if len(argv) > 1 else None
     if subreddit_name:
         top_ten(subreddit_name)
     else:
         print("Please provide a subreddit name as a command-line argument.")
-
