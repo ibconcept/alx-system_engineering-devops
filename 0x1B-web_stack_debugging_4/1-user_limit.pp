e5c5ffd11ebb5e2a33c95104e95aeb90804391e6
William Maina <willima09@gmail.com>
2024-06-13 16:52:41 -0700

    update code

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,28 @@
-# Change the OS configuration so that it is possible to login with the
-# holberton user and open a file without any error message.
+# Puppet manifest to adjust file descriptor limits for holberton user
 
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+# Define the exec resource to update limits.conf
+exec { 'update_limits_conf':
+  command => 'echo "holberton soft nofile 10000" >> /etc/security/limits.conf && echo "holberton hard nofile 10000" >> /etc/security/limits.conf',
+  path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+  unless  => 'grep -E "^holberton[[:space:]]+hard[[:space:]]+nofile[[:space:]]+10000" /etc/security/limits.conf',
 }
+
+# Define the exec resource to reload sysctl settings
+exec { 'reload_sysctl':
+  command     => 'sysctl -p',
+  refreshonly => true,
+  subscribe   => Exec['update_limits_conf'],
+}
+
+# Define the user resource for holberton user
+user { 'holberton':
+  ensure => present,
+}
+
+# Define the file resource for .bash_profile
+file { '/home/holberton/.bash_profile':
+  ensure  => file,
+  owner   => 'holberton',
+  content => "# .bash_profile\n# Get the aliases and functions\nif [ -f ~/.bashrc ]; then\n\t. ~/.bashrc\nfi\n\n# User specific environment and startup programs\nPATH=$PATH:$HOME/bin\nexport PATH",
+}
+
