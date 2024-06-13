346ef97cb42f3fa50a9b9b11c98589b47b871236
William Maina <willima09@gmail.com>
2024-06-13 16:59:43 -0700

    update code

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,4 @@
 exec { 'modify_max_open_files_limit_setting':
-  command => "sed -i 's/#\\s*worker_rlimit_nofile\\s*1024;/worker_rlimit_nofile 10000;/' /etc/nginx/nginx.conf && service nginx restart",
+  command => 'sed -i "s/# worker_rlimit_nofile 1024;/worker_rlimit_nofile 10000;/" /etc/nginx/nginx.conf && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => "grep -q '^\\s*worker_rlimit_nofile\\s*10000;' /etc/nginx/nginx.conf || echo 'worker_rlimit_nofile 10000;' >> /etc/nginx/nginx.conf",
-  require => Package['nginx'],
 }
-
